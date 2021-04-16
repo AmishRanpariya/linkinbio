27aa50c2bc13ad0e28887bf4087c15ca8579983c
amishranpariya <amishranpariya@gmail.com>
2021-04-16 17:35:55 +0530

    made pwa

---
+++ app.js
@@ -0,0 +1,7 @@
+//for PWA
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker
+		.register("sw.js")
+		.then((reg) => console.log("service worker registered"))
+		.catch((err) => console.log("service worker not registered", err));
+}
---
+++ sw.js
@@ -0,0 +1,14 @@
+// install event
+self.addEventListener("install", (evt) => {
+	// console.log('service worker installed');
+});
+
+// activate event
+self.addEventListener("activate", (evt) => {
+	// console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener("fetch", (evt) => {
+	// console.log('fetch event', evt);
+});
