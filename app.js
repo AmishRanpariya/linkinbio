c1612fcf093204fbc685d19149162aa4f8326332
amishranpariya <amishranpariya@gmail.com>
2021-04-17 18:10:58 +0530

    pwa updated

--- app.js
+++
@@ -1,7 +0,0 @@
-//for PWA
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker
-		.register("sw.js")
-		.then((reg) => console.log("service worker registered"))
-		.catch((err) => console.log("service worker not registered", err));
-}
