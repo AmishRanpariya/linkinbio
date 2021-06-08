458eceaa6464be2400733a33d0e5b541c8e0372f
amishranpariya <amishranpariya@gmail.com>
2021-06-08 19:15:09 +0530

    db changed

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".container");
-db.collection("posts")
-	.orderBy("createdAt", "desc")
-	.onSnapshot((snap) => {
-		container.innerHTML = "";
-		snap.forEach((doc) => {
-			let post = doc.data();
+
+fetch("./asset/db.json")
+	.then((res) => {
+		return res.json();
+	})
+	.then((data) => {
+		data.posts.sort((a, b) => b.createdAt - a.createdAt);
+		data.posts.forEach((post) => {
 			container.innerHTML += `
 			<a class="post-div" href=${post.link}  target="_blank">
 				<img src=${post.url} loading="lazy" alt="project image" />
